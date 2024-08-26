91bdb665b8bf68d830220434e6d100e23f9ba28b
suren200320 <hakyan.suren@gmail.com>
2024-08-26 22:53:46 +0400

    created test request

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: process.env.VUE_APP_WEATHER_API_URL,
   params: {
-    appid: VUE_APP_WEATHER_API_KEY
+    appid: process.env.VUE_APP_WEATHER_API_KEY
   },
   timeout: 10000,
   headers: {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,5 +4,13 @@
   </div>
 </template>
 
-<script>
+<script setup>
+import axios from '@/axios';
+import { onMounted } from 'vue';
+
+
+onMounted(async () => {
+  const resp = await axios.get("/weather", { params: { lat: "40.177200", lon: "44.503490" } });
+  console.log(resp);
+})
 </script>
